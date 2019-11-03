782f4b979b2600954fddd03dcf7874225d0181c4
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-03 16:50:40 +0200

    Fixing problems with buttonStyle() and layoutPriority()

---
+++ LoadingASpecificKindOfCodableData/AstronautView.swift
@@ -0,0 +1,41 @@
+//
+//  AstronautView.swift
+//  LoadingASpecificKindOfCodableData
+//
+//  Created by Ramill Ibragimov on 03.11.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    
+    let astronaut: Astronaut
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+                    
+                    Text(self.astronaut.description)
+                        .padding()
+                        .layoutPriority(1)
+                    
+                }
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}
--- LoadingASpecificKindOfCodableData/MissionView.swift
+++ LoadingASpecificKindOfCodableData/MissionView.swift
@@ -33,25 +33,28 @@ struct MissionView: View {
                     
                     ForEach(self.astronauts, id: \.role) {
                         crewMember in
-                        HStack {
-                            Image(crewMember.astronaut.id)
-                                .resizable()
-                                .frame(width: 83, height: 60)
-                                .clipShape(Capsule())
-                                .overlay(Capsule()
-                                .stroke(Color.primary, lineWidth: 1))
-                            
-                            VStack(alignment: .leading) {
-                                Text(crewMember
-                                    .astronaut.name)
-                                    .font(.headline)
+                        NavigationLink(destination: AstronautView(astronaut: crewMember.astronaut)) {
+                            HStack {
+                                Image(crewMember.astronaut.id)
+                                    .resizable()
+                                    .frame(width: 83, height: 60)
+                                    .clipShape(Capsule())
+                                    .overlay(Capsule()
+                                    .stroke(Color.primary, lineWidth: 1))
                                 
-                                Text(crewMember.role)
-                                    .foregroundColor(.secondary)
+                                VStack(alignment: .leading) {
+                                    Text(crewMember
+                                        .astronaut.name)
+                                        .font(.headline)
+                                    
+                                    Text(crewMember.role)
+                                        .foregroundColor(.secondary)
+                                }
+                                Spacer()
                             }
-                            Spacer()
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
+                    .buttonStyle(PlainButtonStyle())
                     }
                     
                     Spacer(minLength: 25)
