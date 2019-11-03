7cfe1939e7afa1d05a19bdf9fb5af8399b7d6997
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-03 16:08:24 +0200

    Showing mission details with ScrollView and GeometryReader

---
+++ LoadingASpecificKindOfCodableData/MissionView.swift
@@ -0,0 +1,42 @@
+//
+//  MissionView.swift
+//  LoadingASpecificKindOfCodableData
+//
+//  Created by Ramill Ibragimov on 03.11.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    
+    let mission: Mission
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.7)
+                        .padding(.top)
+                    
+                    Text(self.mission.description)
+                        .padding()
+                    
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0])
+    }
+}
